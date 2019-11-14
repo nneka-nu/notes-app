a4ffdf30ddf6eb93ad7cee7923dfd0dbb4057b7a
Nneka <nneka.dev@gmail.com>
2019-11-14 04:57:19 -0800

    order notes by date in descending order

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,11 +1,13 @@
 import { createSelector } from 'reselect';
+import moment from 'moment';
 
 const notesSelector = state => state.notes;
 const selectedFolderIdSelector = state => state.selectedFolderId;
 export const getNotesByFolder = createSelector(
   [notesSelector, selectedFolderIdSelector],
   (notes, selectedFolderId) => {
-    return notes.filter(note => note.folderId === selectedFolderId);
+    return notes.filter(note => note.folderId === selectedFolderId)
+                .sort((a, b) => moment(b.lastUpdated).diff(moment(a.lastUpdated)));
   }
 );
 
@@ -13,8 +15,8 @@ const searchSelector = state => state.search;
 export const getNotesBySearch = createSelector(
   [notesSelector, searchSelector],
   (notes, search) => {
-    return notes.filter(note => {
-      return note.noteAsText.toLowerCase().includes(search.term.toLowerCase());
-    });
+    return notes.filter(note => 
+      note.noteAsText.toLowerCase().includes(search.term.toLowerCase())
+    )
   }
 );
